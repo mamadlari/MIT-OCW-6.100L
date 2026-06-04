559422e158bdf5a7cf32c150e26261b9bf8efeb1
Mohammad Tolooei <mamadlarestan@gmail.com>
2026-06-04 20:44:55 +0330

    style:Change Take user's input code location

--- ps1a.py
+++ ps1a.py
@@ -7,16 +7,17 @@
 ## Get user input for yearly_salary, portion_saved and cost_of_dream_home below ##
 ##################################################################################
 
-
-#########################################################################
-## Initialize other variables you need (if any) for your program below ##
-#########################################################################
-
 ## Take input yearly salary,portion saved,cost of dream home from <USER> ##
 
 yearly_salary=float(input("What is your annual salary ? : "))
 portion_saved=float(input("what percentage of your annual salary do you want to save ? : "))
 cost_of_dream_home=float(input("How much does your deam house cost ? : "))
+
+#########################################################################
+## Initialize other variables you need (if any) for your program below ##
+#########################################################################
+
+
 
 ###############################################################################################
 ## Determine how many months it would take to get the down payment for your dream home below ## 
