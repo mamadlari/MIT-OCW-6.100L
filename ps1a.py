962e5464a71f69f88c0c1899d9694cb49c84177e
Mohammad Tolooei <mamadlarestan@gmail.com>
2026-06-07 22:16:40 +0330

    docs: Add explantion about calculating the number of months

--- ps1a.py
+++ ps1a.py
@@ -31,6 +31,8 @@ r=0.05
 ## Determine how many months it would take to get the down payment for your dream home below ## 
 ###############################################################################################
 
+## Calculating monthly savings ##
+
 months=0
 while amount_saved < portion_down_payment:
     amount_saved += amount_saved*(r/12) + (portion_saved*yearly_salary)/12
