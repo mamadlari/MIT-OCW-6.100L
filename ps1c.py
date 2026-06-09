837bf8e08ab65e050ac6f7174a36e251041f3e96
Mohammad Tolooei <mamadlarestan@gmail.com>
2026-06-09 18:22:06 +0330

    refactor:Part c , Optimizing recursive functions and preventing potential infinite loop errors.

--- ps1c.py
+++ ps1c.py
@@ -103,16 +103,8 @@ def Acceptable_r(high,low,steps):
          (r,steps):("The acceptable anuual interest rate" , "number of function executions")
      """
      steps+=1
-     
-     if initial_deposit>=portion_down_payment:
-        return (0.0,steps)
-    
      r=(low+high)/2.0
      ans=Anwser(r)
-     
-     if r==1 and ans!="Acceptable":
-         return (None,steps)
-     
      match ans:
          case "Acceptable":
              return (r,steps)
@@ -125,6 +117,11 @@ def Acceptable_r(high,low,steps):
          case _:
              return (None,steps)
          
-            
-(r,steps)=Acceptable_r(1.0, 0.0,steps)
-print("r = ",r,"and steps in bisections search is : ",steps)
+  
+if initial_deposit>=portion_down_payment:
+   print("r = ",0.0,"and steps in bisections search is : ",steps)
+elif Anwser(1)=="less_than":
+    print("r = ",None,"and steps in bisections search is : ",steps)
+else:
+    (r,steps)=Acceptable_r(1.0, 0.0,steps)
+    print("r = ",r,"and steps in bisections search is : ",steps)
