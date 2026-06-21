5b0786aad21faaa10ab1c117c7fbd63af34fce31
Mohammad Tolooei <mamadlarestan@gmail.com>
2026-06-21 13:39:34 +0330

    feat:add tester files

---
+++ ps1b_in_function.py
@@ -0,0 +1,27 @@
+def part_b(yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise):
+	#########################################################################
+	
+	## Initialize  house down payment , interest and monthly saving ##
+	
+	portion_down_payment=0.25*cost_of_dream_home
+	amount_saved=0.0
+	r=0.05
+	
+	###############################################################################################
+	## Determine how many months it would take to get the down payment for your dream home below ## 
+	###############################################################################################
+	
+	## Calculating monthly savings ##
+	
+	months=0
+	while amount_saved < portion_down_payment:
+	    amount_saved += amount_saved*(r/12) + (portion_saved*yearly_salary)/12
+	    months+=1
+	    
+	## Salary incease every 6 months ##
+	    
+	    if months%6 == 0 :
+	        yearly_salary+=(semi_annual_raise*yearly_salary)
+	    
+	print("after",months,"months later you can buy your dream home !! :) ")
+	return months
